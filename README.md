2efc350ac61f4762111119b0b6474487891feb10
crawfy48 <lukasz.mioduszewski@interia.pl>
2015-01-09 23:18:12 +0100

    New terra version (added PBC, vonNeumann etc)
    Terra.js was changed to mainfile.js with new functions, so now:
    - Periodic Boundary Conditions are working
    - Model has vonNeumann neighborhood
    - Simulation can be stopped, played frame by frame or reset
    Other changes unrelated to new version of Terra:
    - I realized that in Metropolis algorithm domains should be chosen
    randomly, not one after another, so now every domain is selected with
    probability 1-randomness (which is a new parameter)
    - Coupling constant can be changed to observe antiferromagnetic behavior
    for J<0
    - magnetisation per spin is now displayed
    - parameters are aligned left to prevent "flickering" when length of
    floating number is changed (e.g. from 0.1 to -0.01)
    - description is correspondingly longer

--- README.md
+++ README.md
@@ -6,11 +6,13 @@ Ising model simulation using terra.js
 terra.js is a Javascript library for biological simulations and cellular automata (visit http://rileyjshaw.com/terra/ for more details). It was used to implement a simple cellular automaton: Ising model of 2D ferromagnetic material, where each cell is a magnetic domain with 2 states: "up" (+1) or "down" (-1), indicated by different colors. The state is changed according to the Metropolis criterion.
 
 The simulation is running in real time, and certain parameters can be adjusted:
+- randomness is the probability of skipping a domain in single animation step (not applying Metropolis criterion to it). Ideally, only one domain should be checked in each step, but that would slow down the simulation. Setting randomness to 0 means checking every domain, like in the previous version.
+- coupling constant describes the interaction between domains: positive value favors the same alignment (up and up or up and down), negative - opposite alignment (up and down). Zero means no interaction at all.
 - temperature is used to calculate the probability of a domain flip (bigger temperature means bigger termal noise, which means more chaotic flipping)
 - external magnetic field favors domains aligned in the same direction as the field (positive field favors "up" domains, negative "down" domains).
 - friction: when the change of energy is calculated for the Metropolis criterion, the friction parameter is always added to it (to prevent artificial continous domain flipping or to simulate histeresis).
 
-ising.html, ising.css and metropolis.gif constitute the webpage, on which the script is placed. terra.min.js is the terra library downloaded from http://rileyjshaw.com/terra/, and the ising.js is the script itself (the most important file).
+ising.html, ising.css and metropolis.gif constitute the webpage, on which the script is placed. mainfile.js is the terra library downloaded from http://cdn.jsdelivr.net/terra/latest/mainfile, and the ising.js is the script itself (the most important file).
 
-Installation is simple: copy all the files to one folder and open index.html.
-A working version is hosted here: http://students.mimuw.edu.pl/~lm320319/ising.html
+Installation is simple: copy all the files to one folder and open ising.html. The content of the gh-pages branch can be viewed online at adress: http://crawfy48.github.io/Ising-model/ising.html
+Polish version (old) is hosted here: http://students.mimuw.edu.pl/~lm320319/ising.html
--- ising.js
+++ ising.js
@@ -1,57 +1,87 @@
 var size=50;  //lattice size is 50x50
-var IsingModel =new terra.Terrarium 
-(size,size,"IsingTerrarium",10,document.getElementById('placeholder'));  
+var IsingModel = new terra.Terrarium
+(size, size,
+{
+  id: 'IsingTerrarium',
+  periodic: true,
+  neighborhood: 'vonNeumann',
+  insertAfter: document.getElementById('placeholder')
+});
+
 var magnetization = 0;
 var counter=0;
-terra.registerCA({  // register cellular automaton
-  type: 'Ising',    // name it Ising
-  colorFn: function ()  // connect domain state (-1 albo 1) with color
-  { // If state=1, cell is colored, if not, it stays white
-	return this.state==1 ? this.color + ',1' : '0,0,0,0';
-  },
-  process: function (neighbors) // process works separately for every domain
-  { // Counter increases with every domain, there is LxL domains
+var N=size*size;
+
+terra.registerCA({
+  type: 'Ising',
+  colorFn: function () { return this.state === 1 ? this.color + ',1' : '0,0,0,0'; },
+  process: function (neighbors, x, y)
+  {
 	counter+=1;
-	if (counter==size*size)
+	if (counter==N)
 	{   // Magnetization is the sum of domains' states
 		document.getElementById("printM").innerHTML = magnetization;
+		document.getElementById("printm").innerHTML = magnetization*1.0/N;
 		counter=magnetization=0;
 	}   // zero M to count it again
 	magnetization +=this.state;
-	// 3 parameters of the model: temperature, external field and friction
-	temp =	parseFloat(document.getElementById("T").value);
-	field = parseFloat(document.getElementById("H").value);
-	friction = parseFloat(document.getElementById("F").value);
-	document.getElementById("printT").innerHTML = temp;
-	document.getElementById("printH").innerHTML = field;
-	document.getElementById("printF").innerHTML = friction;
-	deltaE = 0;  // Change of energy in case of domain flip
-	for (var i = 0; i < neighbors.length; i++) 
-		deltaE += neighbors[i].creature.state;
-	// only neighbors determine the energy change
-	if (neighbors.length<8)
-		deltaE *= 8/neighbors.length;
-	// instead of periodic boundary conditions I re-scale
-	// deltaE like the domain had 8 neighbors
-	deltaE += field;       // external field influence
-	deltaE *= this.state;  // everything is counted in respect to the domain
-	deltaE += friction;    // friction is added as a "potential step"
-	if (deltaE<0)          // Metropolis criterion
-		this.state *= -1;  // domain flipping
-	else if (Math.random() < Math.exp(-deltaE*0.5/temp))
-	{  // Every constant, like Boltzmann's k, is equal to 1 here
-		this.state *= -1;  // domain flipping
+	randomness = parseFloat(document.getElementById("R").value);
+	if (Math.random() > randomness)
+	{
+		// 3 parameters of the model: temperature, external field and friction
+		temp =	parseFloat(document.getElementById("T").value);
+		field = parseFloat(document.getElementById("H").value);
+		friction = parseFloat(document.getElementById("F").value);
+		coupling = parseFloat(document.getElementById("J").value);
+		document.getElementById("printJ").innerHTML = coupling;
+		document.getElementById("printT").innerHTML = temp;
+		document.getElementById("printH").innerHTML = field;
+		document.getElementById("printF").innerHTML = friction;
+		deltaE = 0;  // Change of energy in case of domain flip
+		for (var i = 0; i < neighbors.length; i++) 
+			deltaE += neighbors[i].creature.state;
+		// only neighbors determine the energy change
+		deltaE *= coupling*this.state;  // everything is counted in respect to the domain
+		deltaE += field*this.state;       // external field influence
+		deltaE += friction;    // friction is added as a "potential step"
+		if (deltaE<0)          // Metropolis criterion
+			this.state *= -1;  // domain flipping
+		else if (Math.random() < Math.exp(-deltaE*0.5/temp))
+		{  // Every constant, like Boltzmann's k, is equal to 1 here
+			this.state *= -1;  // domain flipping
+		}
 	}
+	return true;
   }
-}, 
-function ()  // function for making new cells
-{
-	this.alive=true;  // Just in case it makes any difference
-	if (Math.random() > 0.5)
-		this.state = 1
+}, function () {
+ if (Math.random() > 0.5)
+		this.state = 1;
 	else     // Initial state is chosen randomly
-		this.state = -1
+		this.state = -1;
 });
-// Make the grid and animate it
+
 IsingModel.grid = IsingModel.makeGrid('Ising');
 IsingModel.animate();
+function next()
+{
+	IsingModel.animate(1);
+}
+function play()
+{
+	IsingModel.animate();
+}
+function stop()
+{
+	IsingModel.stop();
+}
+function reset()
+{
+	parseFloat(document.getElementById("R").value=0.9);
+	document.getElementById("J").value = 1.0;
+	document.getElementById("T").value = 0.2;
+	document.getElementById("H").value = 0.0;
+	document.getElementById("F").value = 0.0;
+	IsingModel.stop();
+	IsingModel.grid = IsingModel.makeGrid('Ising');
+	IsingModel.animate(1);
+}
